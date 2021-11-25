261c9b8c4d05c215620436f50e380770d432e7c8
wilc0519 <wilc0519@gmail.com>
2021-11-25 02:08:08 -0500

    pruebas y refactor para las coincidencias

--- solution/__tests__/calcular-coincidencias.test.ts
+++ solution/__tests__/calcular-coincidencias.test.ts
@@ -13,35 +13,50 @@ const rene = new Empleado("RENE", [reneLunes, reneMartes, reneJueves, reneSabado
 const astridLunes = new HorasTrabajasPorDia("MO", 1000, 1200)
 const astridJueves = new HorasTrabajasPorDia("TH", 1200, 1400)
 const astridDomingo = new HorasTrabajasPorDia("SU", 2000, 2100)
-const astrid = new Empleado("Astrid", [astridLunes, astridJueves, astridDomingo])
+const astrid = new Empleado("ASTRID", [astridLunes, astridJueves, astridDomingo])
 
-    
+'ANDRES=MO10:00-12:00,TH12:00-14:00,SU20:00-21:00'
 
-test.only('Astrid y Rene trabajaron 2 horas en el mismo horario y dia', () => {
+const andresLunes = new HorasTrabajasPorDia("MO", 1000,1200)
+const andresJueves = new HorasTrabajasPorDia("TH", 1200, 1400)
+const andresDomingo = new HorasTrabajasPorDia("SU", 2000, 2100)
+
+const andres = new Empleado('ANDRES', [andresLunes, andresJueves, andresDomingo])
+
+test('Astrid y Rene trabajaron 2 horas en el mismo horario y dia', () => {
     const empleados = [rene, astrid]
 
     const resultado = buscarCoindidenciasDeEmpleados(empleados)
 
-    expect(resultado).toEqual("ASTRID-RENE: 2")
-});
-
-/*
-
-test('Astrid y Andres trabajaron 2 horas en el mismo horario y dia', () => {
-
-    // poner lo de andres y hacer todas las pruebas
-    const empleados = [andres, astrid]
-
-    const resultado = buscarCoindidenciasDeEmpleados(empleados)
-    expect(resultado).toEqual("ASTRID-RENE: 2")
+    expect(resultado).toEqual(["RENE-ASTRID: 2"])
 });
 
 
-test('Rene y Andres trabajaron 2 horas en el mismo horario y dia', () => {
+
+test('Rene y Andres coincidieron 2 veces en el horario de trabajo', () => {
 
     // poner lo de andres y hacer todas las pruebas
     const empleados = [rene, andres]
 
     const resultado = buscarCoindidenciasDeEmpleados(empleados)
-    expect(resultado).toEqual("ASTRID-RENE: 2")
-});*/
+    expect(resultado).toEqual(["RENE-ANDRES: 2"])
+});
+
+test('Astrid y Andres coincidieron 2 veces en el horario de trabajo', () => {
+
+    // poner lo de andres y hacer todas las pruebas
+    const empleados = [astrid, andres ]
+
+    const resultado = buscarCoindidenciasDeEmpleados(empleados)
+    expect(resultado).toEqual(["ASTRID-ANDRES: 3"])
+});
+
+test('Retorna todos', () => {
+
+    // poner lo de andres y hacer todas las pruebas
+    const empleados = [rene, astrid, andres ]
+
+    const resultado = buscarCoindidenciasDeEmpleados(empleados)
+    console.log(resultado)
+    expect(resultado).toEqual(['RENE-ASTRID: 2', 'RENE-ANDRES: 2', 'ASTRID-ANDRES: 3'])
+});
